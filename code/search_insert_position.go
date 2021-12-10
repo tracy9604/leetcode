5e2cf46dd253efb7cff767b75b93ba193780bd69
Dory Nguyen <haubeogithub1996@gmail.com>
2021-12-10 11:12:56 +0700

    Search insert position

--- code/search_insert_position.go
+++ code/search_insert_position.go
@@ -1,3 +1,29 @@
 package code
 
+func SearchInsertPosition(nums []int, target int) int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= target {
+			return i
+		} else if i == len(nums)-1 {
+			return i + 1
+		}
+	}
+	return 0
+}
 
+func SearchInsertPositionLog(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[left] < target {
+			left++
+		} else {
+			right--
+		}
+	}
+	return left
+}
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	rs := code.StrStr("hello", "ll")
+	rs := code.SearchInsertPositionLog([]int{1, 3, 5, 6}, 2)
 	fmt.Println(rs)
 
 }
