7e80a3af3484f3db142298380a40bb782e5ffd7d
Dory Nguyen <haubeogithub1996@gmail.com>
2021-12-10 17:49:56 +0700

    Larger integer

---
+++ code/large_integer.go
@@ -0,0 +1,16 @@
+package code
+
+func LargeInteger(digits []int) []int {
+	n := len(digits)
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i] += 1
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	var a = make([]int, n+1)
+	a[0] = 1
+	return a
+}
