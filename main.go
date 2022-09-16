67c2d063567ad0f406bc3a885d198ccaf73a4f65
haunt <hau.nguyen1@be.com.vn>
2022-09-16 09:51:20 +0700

    sort algorithms

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 func main() {
 	//[2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31]
 	//[2,42,38,0,43,21]
-	arr1 := []int{2, 21, 43, 38, 0, 42, 33, 7, 24, 13, 12, 27, 12, 24, 5, 23, 29, 48, 30, 31}
-	arr2 := []int{2, 42, 38, 0, 43, 21}
-	fmt.Println(code.RelativeSortArray(arr1, arr2))
+	arr1 := []int{37, 12, 28, 9, 100, 56, 80, 5, 12}
+	fmt.Println(code.ArrayRankTransform(arr1))
 }
