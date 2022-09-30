2bfe44b3ff4217ddc9b11a095d976bf0431cff79
haunt <hau.nguyen1@be.com.vn>
2022-09-30 10:48:08 +0700

    sort algorithms

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"leetcode/h3_demo"
+	"leetcode/code"
 )
 
 func main() {
-	lat := 21.016492249180757
-	long := 105.79668023976954
+	//lat := 21.016492249180757
+	//long := 105.79668023976954
+	//
+	//indexes := h3_demo.GetIndex(lat, long)
+	//
+	//rs := h3_demo.GetStrGeoFromH3Indexes(indexes)
+	//fmt.Println(rs)
 
-	indexes := h3_demo.GetIndex(lat, long)
-
-	rs := h3_demo.GetStrGeoFromH3Indexes(indexes)
-	fmt.Println(rs)
+	nums := []int{7, 7, 7, 7}
+	fmt.Println(code.SmallerNumbersThanCurrent(nums))
 }
