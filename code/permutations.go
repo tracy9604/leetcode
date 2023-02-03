e8d58dddb0486d559e7201d227bcd8b4fbdaef73
haunt <hau.nguyen1@be.com.vn>
2023-02-03 17:15:21 +0700

    jump game 2

--- code/permutations.go
+++ code/permutations.go
@@ -1,5 +1,8 @@
 package code
 
+import "sort"
+
+// backtracking
 func permutation(nums []int, rs *[][]int, left, right int) {
 	if left == right {
 		ansItem := make([]int, 0)
@@ -21,6 +24,43 @@ func permutation(nums []int, rs *[][]int, left, right int) {
 
 func FindPermutations(nums []int) [][]int {
 	rs := make([][]int, 0)
+	result := make([][]int, 0)
+	sort.Ints(nums)
 	permutation(nums, &rs, 0, len(nums)-1)
+	dupMap := make(map[[8]int]int)
+	for _, item := range rs {
+		itemArr := [8]int{}
+		for i, it := range item {
+			itemArr[i] = it
+		}
+		if _, found := dupMap[itemArr]; !found {
+			dupMap[itemArr] = 1
+			result = append(result, append([]int{}, item...))
+		}
+	}
+	return result
+}
+
+// concatenating subarrays
+func permuteSubArrays(nums, subArray []int, res *[][]int) {
+	if len(nums) == 0 {
+		ansItem := make([]int, 0)
+		ansItem = append(ansItem, subArray...)
+		*res = append(*res, ansItem)
+	} else {
+		for i := 0; i < len(nums); i++ {
+			item := nums[i]
+			left := nums[0:i]
+			right := nums[i+1:]
+			rest := append(left, right...)
+			permuteSubArrays(rest, append(subArray, item), res)
+		}
+	}
+}
+
+func FindPermutationsBySubArrays(nums []int) [][]int {
+	rs := make([][]int, 0)
+	subArray := make([]int, 0)
+	permuteSubArrays(nums, subArray, &rs)
 	return rs
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"leetcode/code"
 	"leetcode/h3_demo"
+	"math"
 	"os"
 	"strings"
 )
@@ -82,7 +83,28 @@ func loadStatus(currentStatus uint32, status []uint32) []uint32 {
 	return globalStatusMap[currentStatus]
 }
 
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378100 // Earth radius in METERS
+
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+
+	return 2 * r * math.Asin(math.Sqrt(h))
+}
+
 func main() {
-	nums := []int{1, 2, 3}
+	nums := []int{1, 1, 2}
 	fmt.Println(code.FindPermutations(nums))
 }
